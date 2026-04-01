a7ba9261a72d0baf7fa9fac6e638ea44104757c4
Aytacio Achilles <aytacioachilles@gmail.com>
2026-04-01 11:19:02 +0300

    Initial Commit

---
+++ StepWidget/StepWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepWidgetApp.swift
+//  StepWidget
+//
+//  Created by Aytac Akyildiz on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct StepWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
